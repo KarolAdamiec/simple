8945bf260a79ea2261710a748963f2a1bec7808c
Karol Adamiec <kad16@bskyb.com>
2015-08-12 18:09:53 +0100

    Added hot module reloading and babel loader. Yay ES6/7

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 document.write(require('./src/module1'));
 document.write('</br></br><span>Rendered on: ' + new Date() + '</span>');
+const lambda = (x) => console.log(x);
+
+lambda('Hi there');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: "./index",
+	devtool: 'source-map',
+    entry: ['webpack/hot/dev-server','./index'],
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
-    }
+    },
+    plugins: [
+    	new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+   		loaders: [
+   			{ test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }
+    	]
+  	}
 };
