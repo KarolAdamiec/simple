683bd320bd449a530a5b7ea130da7ef56fcb4a46
Karol Adamiec <kad16@bskyb.com>
2015-08-13 18:02:44 +0100

    Formatting

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -4,36 +4,36 @@
 */
 var webpack = require('webpack');
 var fs = require('fs');
-var testFiles = fs.readdirSync('./test').map(function(testFile){
-                      return './test/' + testFile;
-                  });
+var testFiles = fs.readdirSync('./test').map(function (testFile) {
+  return './test/' + testFile;
+});
 
 //Trick to not bundle node_modules and use them in node with require()
 var nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function(x) {
+  .filter(function (x) {
     return ['.bin'].indexOf(x) === -1;
   })
-  .forEach(function(mod) {
+  .forEach(function (mod) {
     nodeModules[mod] = 'commonjs ' + mod;
   });
-  nodeModules['react/addons'] = 'commonjs react/addons';
-  
+nodeModules['react/addons'] = 'commonjs react/addons';
+
 module.exports = {
-  entry:  testFiles,
+  entry: testFiles,
   target: 'node',
-    
+
   output: {
-        path: __dirname + '/dist',
-        filename: 'test.bundle.js'
+    path: __dirname + '/dist',
+    filename: 'test.bundle.js'
   },
   externals: nodeModules,
   plugins: [
-    	new webpack.NoErrorsPlugin(),
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
     ]
-  },   
+  },
 };
