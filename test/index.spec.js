2478b327bdda61a05590ea6acb5acd29091eecb9
Karol Adamiec <kad16@bskyb.com>
2015-08-13 17:45:31 +0100

    Testing bundle dose not include dependencies.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,6 @@
 import test from 'tape';
+import createComponent from './create-component';
+//import index from '../index';
 
 test('index', function (t) {
     t.plan(1);
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,27 +1,39 @@
 /*
 * ES5 syntax only here...
-* Rename the file to webpack.config.babel.js for ES6/7
+* Rename the file to webpack.test.config.babel.js for ES6/7
 */
 var webpack = require('webpack');
 var fs = require('fs');
-var entryArray = fs.readdirSync('./test').map(function(testFile){
+var testFiles = fs.readdirSync('./test').map(function(testFile){
                       return './test/' + testFile;
                   });
 
+//Trick to not bundle node_modules and use them in node with require()
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+  nodeModules['react/addons'] = 'commonjs react/addons';
+  
 module.exports = {
-	devtool: 'eval',
+  entry:  testFiles,
   target: 'node',
-    entry: entryArray,
-    output: {
-        path: __dirname + "/dist",
-        filename: "testBundle.js"
-    },
-    plugins: [
-    	new webpack.NoErrorsPlugin()
-    ],
-    module: {
-   		loaders: [
-   			{ test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }
-    	]
-  	}
+    
+  output: {
+        path: __dirname + '/dist',
+        filename: 'test.bundle.js'
+  },
+  externals: nodeModules,
+  plugins: [
+    	new webpack.NoErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+    ]
+  },   
 };
